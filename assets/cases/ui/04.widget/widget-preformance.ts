ee7041591a52e1ae1692e86a2b73e037e5ccb0db
zhangmingzhen <mingzhen.zhang@cocos.com>
2021-12-08 13:53:22 +0800

    update widget performance testcase
    (cherry picked from commit ea0153ab8d0b391ebeccfbdcfe96eb8ec316d6f3)

--- assets/cases/ui/04.widget/widget-preformance.ts
+++ assets/cases/ui/04.widget/widget-preformance.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, Prefab, instantiate, Widget, Sprite, Color, Texture2D, SpriteFrame, Label, UITransform } from "cc";
+import { _decorator, Component, Node, Prefab, instantiate, Widget, Sprite, Color, Texture2D, SpriteFrame, Label, UITransform, Button, EditBox, find } from "cc";
 const { ccclass, property , menu} = _decorator;
 
 @ccclass("WidgetPreformance")
@@ -9,17 +9,51 @@ export class WidgetPreformance extends Component {
     public performancePrefab: Prefab = null!;
     @property(SpriteFrame)
     public bgTex: SpriteFrame = null!;
+    @property(Label)
+    public countLabel: Label = null!;
+    @property(Button)
+    public confirmButton: Button = null!;
+    @property(EditBox)
+    public editBox: EditBox = null!;
 
     public nodeA: Node = null!;
 
     start () {
+        this.confirmButton.node.on('click',this.onRefreshNode,this);
+
+        this.onGenerateNode(500);
+        this.refreshCountLabel(500);
+    }
+
+    onRefreshNode() {
+        const countStr = this.editBox.string;
+        const count = Number(countStr);
+        if(!count) {
+            this.countLabel.string = '输入不合法';
+        } else {
+            this.onGenerateNode(count);
+            this.refreshCountLabel(count);
+        }
+    }
+    
+    refreshCountLabel (count:number) {
+        this.countLabel.string = `当前节点数量 ${count}`;
+    }
+
+    onGenerateNode(count:number) {
+        const parent = find('canvas/widget-performance');
+        if(parent) {
+            parent.destroy();
+            console.log('parent is destroyed.');
+        }
+
         let i = 0;
         this.nodeA = instantiate(this.performancePrefab);
         this.node.addChild(this.nodeA);
         const sprite = this.nodeA.getComponent(Sprite)!;
         sprite.spriteFrame = this.bgTex;
         const arr = [true, false];
-        for (i = 0; i < 500; i++) {
+        for (i = 0; i < count; i++) {
             const child = instantiate(this.performancePrefab) as Node;
             child.name = `layer_${i + 1}`;
             this.nodeA.addChild(child);
@@ -38,7 +72,7 @@ export class WidgetPreformance extends Component {
             renderComp.color = new Color(Math.random() * 255, Math.random() * 255, Math.random() * 255, 255);
         }
 
-        this.schedule(this.adjustWidget,0.5);
+        this.schedule(this.adjustWidget,2);
     }
 
     onDisable () {
